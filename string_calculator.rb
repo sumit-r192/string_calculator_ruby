38c240996990dafb88410e902aa39c0114fd732f
Sumit <sumit.raghuwanshi192@gmail.com>
2024-04-30 21:15:24 +0530

    Refactor code

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,6 @@ class StringCalculator
     return 0 if numbers.empty?
 
     response = []
-
     new_delimiter = ','
     numbers.each do |number|
       if number.include?('//')
@@ -11,13 +10,9 @@ class StringCalculator
         number.slice!(0..3)
       end
 
-      raise ArgumentError, "Invalid input: '#{number}'" if number.include?(",\n")
-      raise ArgumentError, "Invalid input: '#{number}'" if number.include?("\n,")
-      raise ArgumentError, "Invalid input: '#{number}'" if number.include?(",,")
-      raise ArgumentError, "Invalid input: '#{number}'" if number.include?(",#{new_delimiter}")
-      raise ArgumentError, "Invalid input: '#{number}'" if number.include?("#{new_delimiter},")
-      raise ArgumentError, "Invalid input: '#{number}'" if number.include?("#{new_delimiter}\n")
-      raise ArgumentError, "Invalid input: '#{number}'" if number.include?("\n#{new_delimiter}")
+      invalid = number.include?(",#{new_delimiter}") || number.include?("#{new_delimiter},") || number.include?("\n#{new_delimiter}") || number.include?("#{new_delimiter}\n") || number.include?(",,") || number.include?(",\n") || number.include?("\n,")
+
+      raise ArgumentError, "Invalid input: '#{number}'" if invalid
       raise ArgumentError, "Negative numbers not allowed: #{number.scan(/-\d+/).join(', ')}" if number.include?("-")
 
       response << number.split(/[,\n"#{new_delimiter}"]/).map(&:to_i).sum
