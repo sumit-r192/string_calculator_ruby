6c2f8a42c329ed2ad5f39cf5d91e1823231fcb0a
Sumit <sumit.raghuwanshi192@gmail.com>
2024-04-30 21:13:58 +0530

    13th test case with validation check for new delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -67,5 +67,11 @@ RSpec.describe StringCalculator do
       calc = StringCalculator.new
       expect(calc.add("//;\n1;2")).to eq(3)
     end
+
+    it "returns an error for invalid input with new delimiter '1;,2'" do
+      calc = StringCalculator.new
+      expect { calc.add("//;\n1;,2") }.to raise_error(ArgumentError, "Invalid input: '1;,2'")
+    end
+
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,21 +2,27 @@ class StringCalculator
   def add(*numbers)
     return 0 if numbers.empty?
 
+    response = []
+
     new_delimiter = ','
     numbers.each do |number|
       if number.include?('//')
         new_delimiter = number[2]
         number.slice!(0..3)
       end
-    end
 
-    numbers.each do |number|
       raise ArgumentError, "Invalid input: '#{number}'" if number.include?(",\n")
+      raise ArgumentError, "Invalid input: '#{number}'" if number.include?("\n,")
       raise ArgumentError, "Invalid input: '#{number}'" if number.include?(",,")
-      raise ArgumentError, "Negative numbers not allowed: #{number.scan(/-\d+/).join(', ') }" if number.include?("-")
+      raise ArgumentError, "Invalid input: '#{number}'" if number.include?(",#{new_delimiter}")
+      raise ArgumentError, "Invalid input: '#{number}'" if number.include?("#{new_delimiter},")
+      raise ArgumentError, "Invalid input: '#{number}'" if number.include?("#{new_delimiter}\n")
+      raise ArgumentError, "Invalid input: '#{number}'" if number.include?("\n#{new_delimiter}")
+      raise ArgumentError, "Negative numbers not allowed: #{number.scan(/-\d+/).join(', ')}" if number.include?("-")
+
+      response << number.split(/[,\n"#{new_delimiter}"]/).map(&:to_i).sum
     end
 
-    response = numbers.map { |number| number.split(/[,\n, "#{new_delimiter}"]/).map(&:to_i).sum }
     response.length > 1 ? response : response.first
   end
 end
