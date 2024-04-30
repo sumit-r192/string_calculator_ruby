ecf11e19df2ff8bfe73e8d175c45da262735bb4b
Sumit <sumit.raghuwanshi192@gmail.com>
2024-04-30 19:51:52 +0530

    6th test case with '\n' in string with numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,9 +23,14 @@ RSpec.describe StringCalculator do
     end
 
     # Allow the add method to handle an any amount of numbers.
-    it "returns the sum of multiple numbers" do
+    it "returns the sum of multiple numbers in multiple strings" do
       calc = StringCalculator.new
       expect(calc.add("1,2", "1,2,3")).to eq([3, 6])
+    end
+
+    it "returns the sum of multiple numbers separate by \n instead of comma" do
+      calc = StringCalculator.new
+      expect(calc.add("1,2\n3,4\n5")).to eq(15)
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
   def add(*numbers)
     return 0 if numbers.empty?
 
-    response = numbers.map{ |number| number.split(",").map(&:to_i).sum }
+    response = numbers.map { |number| number.split(/[,\n]/).map(&:to_i).sum }
     response.length > 1 ? response : response.first
   end
 end
